b2cf36fac4ff3204a4397194862de865af8f6971
Sahaj <sahajpaun01@gmail.com>
2025-11-25 11:50:21 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+     <h1 className="font-game text-2xl">Welcome to the home page!</h1>
+     <Button>Click me</Button>
+    </>
+  );
+}
